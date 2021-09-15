adda6fffb787ff947c9da8c1f96c7002187e8048
Song-YunMin <thd930308@naver.com>
2021-09-15 17:01:46 +0900

    Server Init

---
+++ apollo-instance.js
@@ -0,0 +1,43 @@
+const {ApolloServer} = require('apollo-server-express');
+const {PrismaClient} = require('@prisma/client');
+const queries = require('./lib/typeDefs/Query');
+const mutations = require('./lib/typeDefs/Mutation');
+const users = require('./lib/resolver/users');
+const feeds = require('./lib/resolver/feeds');
+const schema = require('./lib/schema/_schema');
+const prisma = new PrismaClient();
+
+const typeDefs = [
+    queries,
+    mutations,
+    schema.typeDefs
+]
+
+const resolvers = [
+    users.resolvers,
+    feeds.resolvers
+]
+const testServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => {
+        const {request: req} = require('express')
+        // req.headers['authorization'] = `Bearer ${process.env.JWT_TEST_TOKEN}`
+        return {
+            req, prisma
+        }
+    }
+})
+
+
+const apollo = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({req}) => {
+        return {
+            req, prisma
+        }
+    }
+})
+
+module.exports = {apollo, testServer}
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const apolloInstance = require('./apollo-instance');
+const cookieParser = require('cookie-parser');
+const port = 3000
+require('dotenv').config();
+
+app.use(cookieParser());
+
+apolloInstance.apollo.applyMiddleware({
+    app,
+    cors: {
+        origin: 'http://localhost:8080',
+        credentials: true
+    }
+})
+
+apolloInstance.testServer.applyMiddleware({
+    app
+})
+
+const connect = app.listen(port, () => {
+    console.log("Apollo Server start.");
+})
+
+const disconnect = () => {
+    connect.close();
+}
+
+module.exports = {app, disconnect}
